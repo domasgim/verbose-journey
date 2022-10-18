b6a948cb9d084c89c2da02a58a52c910aec2bfda
domasgim <dominykas.gimzunas@protonmail.com>
2022-10-18 18:33:09 +0300

    update

---
+++ mem.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+#include <windows.h>
+#include <TlHelp32.h>
+
+namespace mem
+{
+
+	void PatchEx(BYTE* dst, BYTE* src, unsigned int size, HANDLE hProcess);
+	void NopEx(BYTE* dst, unsigned int size, HANDLE hProcess);
+}
